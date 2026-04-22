cc9d450486fde9de5b119ef6dd554ff7c40fd2ac
TyRichards <me@tyrichards.com>
2026-04-22 02:32:47 -0500

    Lower color palette contrast

--- neovim.lua
+++ neovim.lua
@@ -6,45 +6,58 @@ return {
 		priority = 1000,
 		opts = {
 			colors = {
-				bg = "#000000",
-				dark_bg = "#000000",
-				darker_bg = "#000000",
-				lighter_bg = "#000000",
+				bg = "#130c0b",
+				dark_bg = "#130c0b",
+				darker_bg = "#130c0b",
+				lighter_bg = "#130c0b",
 
-				fg = "#f6b389",
+				fg = "#e2a680",
 				dark_fg = "#b98667",
 				light_fg = "#f7be9b",
 				bright_fg = "#f8c6a7",
-				muted = "#433c3c",
+				muted = "#685050",
 
-				red = "#418fd7",
+				red = "#25829b",
 				yellow = "#db5f0c",
-				orange = "#418fd7",
+				orange = "#25829b",
 				green = "#75553d",
-				cyan = "#f6b389",
-				blue = "#ec850c",
-				purple = "#ec850c",
+				cyan = "#e2a680",
+				blue = "#d7790b",
+				purple = "#d7790b",
 				brown = "#386085",
 
-				bright_red = "#ec850c",
+				bright_red = "#d7790b",
 				bright_yellow = "#db5f0c",
 				bright_green = "#c52a21",
-				bright_cyan = "#bb825d",
-				bright_blue = "#ec850c",
-				bright_purple = "#9c7151",
+				bright_cyan = "#b07a57",
+				bright_blue = "#d7790b",
+				bright_purple = "#936a4c",
 
 				accent = "#c52a21",
-				cursor = "#f6b389",
-				foreground = "#f6b389",
-				background = "#000000",
-				selection = "#000000",
-				selection_foreground = "#f6b389",
-				selection_background = "#000000",
+				cursor = "#e2a680",
+				foreground = "#e2a680",
+				background = "#130c0b",
+				selection = "#130c0b",
+				selection_foreground = "#e2a680",
+				selection_background = "#130c0b",
 			},
 		},
 		-- set up hot reload
 		config = function(_, opts)
 			require("aether").setup(opts)
+			local c = opts.colors
+			local function apply_overrides()
+				vim.api.nvim_set_hl(0, "@variable", { fg = c.fg })
+				vim.api.nvim_set_hl(0, "@variable.lua", { fg = c.fg })
+				vim.api.nvim_set_hl(0, "@property", { fg = c.bright_cyan })
+				vim.api.nvim_set_hl(0, "@property.lua", { fg = c.bright_cyan })
+				vim.api.nvim_set_hl(0, "Identifier", { fg = c.bright_cyan })
+				vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg = c.accent, underline = true })
+			end
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				pattern = "aether",
+				callback = apply_overrides,
+			})
 			vim.cmd.colorscheme("aether")
 			require("aether.hotreload").setup()
 		end,
